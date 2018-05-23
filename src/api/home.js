39c5132f5850bd6b5146d20f767c67cacd47259c
xiaofei <lixiaofei0627@gmail.com>
2018-05-23 13:55:00 +0800

    我的页面完成

--- src/api/home.js
+++ src/api/home.js
@@ -3,7 +3,7 @@ export default class HomeServer {
     // 获取城市列表https://api.yooqun.com/v1/pc_admin/groups/?page=1&perPage=20
     // https://api.yooqun.com/v1/pc_admin/events/?page=1&perPage=20
     // /v1/pc_admin/users/https://api.yooqun.com/v1/pc_admin/events/528/members/
-
+    //获取菜单
     static getMenu(params) {
         return request({
             url: '/v2/index_entry',
@@ -11,14 +11,24 @@ export default class HomeServer {
             params: params
         })
     }
-
+    // 获取商铺列表
     static getShoppingList(params) {
         // 示例：
         // http://cangdu.org:8001/shopping/restaurants?latitude=31.22967&longitude=121.4762
         return request({
-            url:'/shopping/restaurants?latitude=31.22967&longitude=121.4762',
+            url: '/shopping/restaurants?latitude=31.22967&longitude=121.4762',
             method: 'get',
             params: params
+        })
+    }
+    // 获取商铺详情    
+    static getShoppingInfo(id) {
+        // 示例：
+        // http://cangdu.org:8001/shopping/restaurants?latitude=31.22967&longitude=121.4762
+        return request({
+            url: `/shopping/restaurants/${id}`,
+            method: 'get',
+
         })
     }
 
--- src/pages/my/my.jsx
+++ src/pages/my/my.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { NavBar } from 'antd-mobile';
 import MyTabar from '../common/MyTabar';
+import { Link } from 'react-router-dom'
+
 import './my.less';
 class my extends Component {
 
@@ -21,10 +23,79 @@ class my extends Component {
 
     return (
       <div>
-           <NavBar mode="dark">我的</NavBar>
+        <NavBar mode="dark">我的</NavBar>
+        <div className='my-warp'>
+          <Link to='/' className='profile-link'>
+            <img src="https://img2.woyaogexing.com/2018/05/22/356346955771e83a!400x400_big.jpg" alt="" />
+            <div className='personal-info'>
+              <p>1446628111</p>
+              <p><i className="fa fa-mobile" aria-hidden="true"></i>
+                123456789
+              </p>
+              <i className="fa fa-angle-right" aria-hidden="true"></i>
+            </div>
 
+          </Link>
+
+          <section className="info-data">
+            <ul className="clear">
+              <li className="info-data-link">
+                <p className="info-data-top"><b >10.00</b>元</p>
+                <p className="info-data-bottom">我的余额</p>
+              </li>
+              <li className="info-data-link">
+                <p className="info-data-top"
+                ><b className='second-count'>3</b>个
+              </p>
+                <p className="info-data-bottom">
+                  我的优惠</p>
+              </li>
+              <li className="info-data-link">
+                <p className="info-data-top"
+                ><b >0</b>分</p> <p className="info-data-bottom">
+                  我的积分</p></li>
+            </ul>
+          </section>
+
+        
+          <section class="profile-menu">
+            <div className='profile-menu-item'>
+              <i className="fa fa-list-alt" aria-hidden="true"></i>
+              我的订单
+              <i className="fa fa-angle-right" aria-hidden="true"></i>
+            </div>
+            <div className='profile-menu-item'>
+              <i className="fa fa-shopping-bag" aria-hidden="true"></i>
+              积分商城
+              <i className="fa fa-angle-right" aria-hidden="true"></i>
+            </div>
+            <div className='profile-menu-item'>
+              <i className="fa fa-gift" aria-hidden="true"></i>
+              会员卡
+              <i className="fa fa-angle-right" aria-hidden="true"></i>
+
+            </div>
+
+          </section >
+
+  <section class="profile-menu">
+            <div className='profile-menu-item'>
+              <i className="fa fa-envelope-open" aria-hidden="true"></i>
+            服务中心
+              <i className="fa fa-angle-right" aria-hidden="true"></i>
+            </div>
+            <div className='profile-menu-item'>
+              <i className="fa fa-cloud-download" aria-hidden="true"></i>
+              下载app
+              <i className="fa fa-angle-right" aria-hidden="true"></i>
+            </div>
+     
+
+          </section >
+
+        </div >
         <MyTabar ></MyTabar>
-      </div>
+      </div >
 
     );
   }
